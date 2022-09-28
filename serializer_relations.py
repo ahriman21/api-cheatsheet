a80be2f714c318a8e98548ffc70a94f59ae48275
ahriman <109135045+ahriman21@users.noreply.github.com>
2022-09-28 18:18:01 +0330

    Update serializer_relations.py

--- serializer_relations.py
+++ serializer_relations.py
@@ -1,4 +1,5 @@
-# 1- we can create a serializer for a model and use it in another serializer :
+# 1- SIMPLE SERIALIZERS
+#  we can create a serializer for a model and use it in another serializer :
 class UserSerializer(serializers.Serializer):
 	username = serializers.CharField()
 	email = serializer.EmailField()
@@ -9,8 +10,9 @@ class QuestionSerializer(serializers.Serializer):
 # 
 	model = Question
 	fields = ['title','question','user']
-	
-# 2- or we can use another way to access to other columns of User table :
+# =======================================================================================
+# 2- RELATED FIELDS
+#  or we can use another way to access to other columns of User table :
 
 class QuestionSerializer(serializers.Serializer):
 	user = serializers.SlugRelatedField(slug_field = 'email')
@@ -18,14 +20,19 @@ class QuestionSerializer(serializers.Serializer):
 	model = Question
 	fields = ['title','question','user']
 
-## other relation fields :
+## Other relation fields :
 serializers.PrimaryRelatedField()
 serializers.StringRelatedField()
 
+## Create custom RELATED FIELDS : ===> user = UsernameRelatedField()*
+class UsernameRelatedField(serializers.RelatedField):
+	def to_representation(self,value):
+		return value.username
 
 
-
-# 3- this way is when you have a relation that is list[] or tuple(). for example a list of answers for a question :
+# =======================================================================================
+# 3- METHOD FIELDS
+#  this way is when you have a relation that is list[] or tuple(). for example a list of answers for a question :
 class AnswerSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = Answer
