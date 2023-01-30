a00aa2d11145e6e07525202b9fb320c9abaa6c93
ahriman <109135045+ahriman21@users.noreply.github.com>
2023-01-30 13:15:53 +0330

    Create overwriting_update_create_method_serializer.py

---
+++ overwriting_update_create_method_serializer.py
@@ -0,0 +1,17 @@
+def create(self,validated_data):
+  return User.objects.create_user(**validated_data)
+
+
+
+def update(self,instance,validated_data):
+  # we delete the password from vaidated_data dictionary and set it None because user might not change the password.
+  #- and asign it to password variable to access that later. 
+  #- becuast the password have to be saved using set_password() method.
+  password = validated_data.pop('password',None)  
+  user = super().update(instance,validated_data)
+  
+  if password:
+    user.set_password(password)
+    user.save()
+    
+  return user
