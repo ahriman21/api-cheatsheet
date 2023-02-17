b4ee057f05702ada4cb358a39303352ee8dd2a16
ahriman <109135045+ahriman21@users.noreply.github.com>
2023-02-17 14:10:39 +0330

    Update overwriting_update_create_method_serializer.py

--- overwriting_update_create_method_serializer.py
+++ overwriting_update_create_method_serializer.py
@@ -2,7 +2,12 @@ def create(self,validated_data):
   return User.objects.create_user(**validated_data)
 
 
-
+def update(self,instance,validated_data):
+  instance.username = validated_data('username',instance.username)
+  instance.save()
+  return instance
+#========================================
+# example of updating a user instance.
 def update(self,instance,validated_data):
   # we delete the password from vaidated_data dictionary and set it None because user might not change the password.
   #- and asign it to password variable to access that later. 
